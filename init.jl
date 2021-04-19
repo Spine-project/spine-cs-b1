bdc2cfd20b5ab30aa77720ffa88d690ac8222559
Manuel Marin <manuelma@kth.se>
2021-04-19 18:29:21 +0200

    Keep getting rid of PyCall

--- init.jl
+++ init.jl
@@ -5,13 +5,3 @@ Pkg.activate(dirname(@__FILE__))
 
 # Download and install all required packages
 Pkg.instantiate()
-
-# Set Python executable to current and re-build PyCall if necessary
-ENV["PYTHON"] = Sys.which("python")
-try
-    Pkg.build("PyCall")
-catch e
-    if !isa(e, Pkg.Types.PkgError)
-        throw(e)
-    end
-end
